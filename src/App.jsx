19dcf16e9220607c58b42c710e6e0216683555d9
shadirahdari <ra.rahdari@gmail.com>
2025-04-04 16:32:48 +0300

    Added routing with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from './Components/Navbar/navbar'
+import{ Route, Routes} from 'react-router-dom'
+import Home from './Components/Pages/Home/Home'
+import Cart from './Components/Pages/Cart/Cart'
+import PlaceOrder from '/.Components/Pages/PlaceOrder/PlaceOrder'
+
 
 const App = () => {
   return (
     <div className='App'>
       <Navbar />
+      <Routes>
+      < Route path ='/' element={<Home/>}/>
+      < Route path ='/Cart' element={<Cart/>}/>
+      <Route path='/Order' element={<PlaceOrder/>}/>
+      </Routes>
     </div>
   )
 }
