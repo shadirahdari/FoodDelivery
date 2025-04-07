71cc818fdc6eb651621dc636801765370df828f7
shadirahdari <ra.rahdari@gmail.com>
2025-04-07 19:56:41 +0300

    added StoreContextProvider

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -1,1 +1,17 @@
-export
+import { createContext } from "react";
+
+export const StoreContext=createContext(null)
+const StoreContextProvider = (props)=> {
+    const contextValue= 
+    {
+
+    }
+    return (
+        <StoreContext.Provider value={
+            contextValue}>
+{props.children}
+            </StoreContext.Provider>
+        
+    )
+}
+export default StoreContextProvider;
