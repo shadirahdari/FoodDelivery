a3fedbaeb78ab6e56751f68ad9b39cb7000266a6
shadirahdari <ra.rahdari@gmail.com>
2025-04-11 17:20:04 +0300

    Connect FoodItem to global cart state using StoreContext

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -3,9 +3,29 @@ import { food_list } from "../assets/assets";
 
 export const StoreContext=createContext(null)
 const StoreContextProvider = (props)=> {
+
+    const [cartItems,setCartItem]=useState({});
+
+    const addToCart=(itemId)=> {
+        if (!cartItems[itemId]){
+            setCartItems((prev)=>({...prev,[itemId]:1}))
+        }
+        else{
+            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        }
+    }
+
+    const removeFromCart=()=>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
     const contextValue= 
     {
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
+
 
     }
     return (
