7f267262d5f1145d326be74e3fbbd56cc3ac59ed
shadirahdari <ra.rahdari@gmail.com>
2025-04-16 20:01:05 +0300

    add subtotal and total calculation in Cart using contex

--- src/Components/Pages/Cart/Cart.jsx
+++ src/Components/Pages/Cart/Cart.jsx
@@ -1,9 +1,13 @@
-import React, { useContext } from 'react'
-import './Cart.css'
-import { StoreContext } from '../../../Context/StoreContext'
+import React, { useContext } from 'react';
+import './Cart.css';
+import { StoreContext } from '../../../Context/StoreContext';
 
 const Cart = () => {
-  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
+  const subtotal = getTotalCartAmount();
+  const deliveryFee = 2;
+  const total = subtotal + deliveryFee;
+
   return (
     <div className='cart'>
       <div className="cart-items">
@@ -17,59 +21,58 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-        {food_list.map((item, index) => {
+        {food_list.map((item) => {
           if (cartItems[item._id] > 0) {
             return (
-              <div>
+              <div key={item._id}>
                 <div className='cart-items-title cart-items-item'>
                   <img src={item.image} alt="" />
                   <p>{item.name}</p>
                   <p>${item.price}</p>
                   <p>{cartItems[item._id]}</p>
                   <p>${item.price * cartItems[item._id]}</p>
-                  <p onClick={()=>removeFromCart(item._id)}className='cross'>X</p>
+                  <p onClick={() => removeFromCart(item._id)} className='cross'>X</p>
                 </div>
                 <hr />
               </div>
-            
-
-            )
+            );
           }
+          return null;
         })}
       </div>
+
       <div className="cart-bottom">
         <div className="cart-total">
           <h2>Cart Totals</h2>
-          <div> <div className="cart-total-detials">
-            <p>Subtotal</p>
-            <p>{0}</p>
+          <div>
+            <div className="cart-total-detials">
+              <p>Subtotal</p>
+              <p>${subtotal}</p>
+            </div>
+            <hr />
+            <div className="cart-total-detials">
+              <p>Delivery Fee</p>
+              <p>${deliveryFee}</p>
+            </div>
+            <hr />
+            <div className="cart-total-detials">
+              <b>Total</b>
+              <b>${total}</b>
+            </div>
           </div>
-          <hr />
-          <div className="cart-total-detials">
-            <p>Delivery Fee</p>
-            <p>{2}</p>
-          </div>
-          <hr />
-          <div className="cart-total-detials">
-           <b>Total</b>
-           <b>{0}</b>
+          <button>Proceed to Checkout</button>
         </div>
-       
-      </div>
-      <button>Proceed to Checkout</button>
-    </div>
-    <div className="cart-promocode">
-      <div>
-        <p>If you have a promo code, Enter it here</p>
-        <div className='cart-promocode-input'>
-          <input type="text" placeholder='promo code' />
-          <button>Submit </button>
-        </div>
-      </div>
-    </div>
-    </div>
-    </div>
-  )
-}
 
-export default Cart
+        <div className="cart-promocode">
+          <p>If you have a promo code, Enter it here</p>
+          <div className='cart-promocode-input'>
+            <input type="text" placeholder='promo code' />
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -1,39 +1,54 @@
-import { createContext,useState } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
-export const StoreContext=createContext(null)
-const StoreContextProvider = (props)=> {
+export const StoreContext = createContext(null);
 
-    const [cartItems,setCartItem]=useState({});
+const StoreContextProvider = (props) => {
+  const [cartItems, setCartItem] = useState({});
 
-    const addToCart=(itemId)=> {
-        if (!cartItems[itemId]){
-            setCartItem((prev)=>({...prev,[itemId]:1}))
+  const addToCart = (itemId) => {
+    setCartItem((prev) => ({
+      ...prev,
+      [itemId]: (prev[itemId] || 0) + 1,
+    }));
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItem((prev) => {
+      const newCount = (prev[itemId] || 0) - 1;
+      return {
+        ...prev,
+        [itemId]: newCount > 0 ? newCount : 0,
+      };
+    });
+  };
+
+  const getTotalCartAmount = () => {
+    let total = 0;
+    for (const itemId in cartItems) {
+      if (cartItems[itemId] > 0) {
+        const item = food_list.find((item) => item._id === itemId);
+        if (item) {
+          total += item.price * cartItems[itemId];
         }
-        else{
-            setCartItem((prev)=>({...prev,[itemId]:prev[itemId]+1}))
-        }
+      }
     }
+    return total;
+  };
 
-    const removeFromCart=(itemId)=>{
-        setCartItem((prev)=>({...prev,[itemId]:prev[itemId]-1}))
-    }
-    const contextValue= 
-    {
-        food_list,
-        cartItems,
-        setCartItem,
-        addToCart,
-        removeFromCart
+  const contextValue = {
+    food_list,
+    cartItems,
+    addToCart,
+    removeFromCart,
+    getTotalCartAmount,
+  };
 
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
 
-    }
-    return (
-        <StoreContext.Provider value={
-            contextValue}>
-{props.children}
-            </StoreContext.Provider>
-        
-    )
-}
 export default StoreContextProvider;
